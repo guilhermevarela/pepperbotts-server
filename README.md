9688b500c84f29117b83b8bf87b2020d73f85f5d
Guilherme Varela <guilhermevarela@hotmail.com>
2017-07-19 21:21:51 -0300

    LUIS intent

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ This is a simple bot integrating
 * This is a first attempt to integrate LUIS to NodeJS
 * 0.1 Intents handler
 * [craftworkz](https://blog.craftworkz.co/writing-a-basic-chatbot-using-microsoft-botbuilder-and-luis-9b1e2a01cde1)
+* [bot recognizer](http://docs.botframework.com/builder/node/guides/understanding-natural-language/)
+* [bot alarm sample](https://github.com/Microsoft/BotBuilder/blob/master/Node/examples/basics-naturalLanguage/app.js)
 
 ### Begin incantation by running ###
 ```
--- messages/index.js
+++ messages/index.js
@@ -14,44 +14,62 @@ var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure
 });
 
 var bot = new builder.UniversalBot(connector);
+var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
+
 bot.localePath(path.join(__dirname, './locale'));
+bot.recognizer(recognizer);
+.matches('Info-general', function (session, args) {
+  session.send('You asked for information' + JSON.stringify(args))
+});
+.matches('Make-appointment', function (session, args) {
+  session.send('You asked for appointment' + JSON.stringify(args))
+});
+.onDefault(function (session){ 
+  session.send('Sorry i didint quite catch that')
+});
+// var intents = new builder.IntentDialog( recognizers: [recognizer])
+
 
 // bot.dialog('/', function (session) {
 //     session.send('You said ' + session.message.text);
 // });
 
 //determining the intent of the user is the main objective when building a chatbot (that, and extracting entities, but more on that later)
-var intents = new builder.IntentDialog();
-bot.dialog('/', intents);
-intents.matches(/^say something else/i, [
-    function (session) {
-        session.beginDialog('/changesentence');
-    },
-    function (session, results) {
-        session.send('Ok, I will say the following sentence from now on: %s', session.userData.sentence);
-    }
-])
-intents.onDefault([
-  function (session, args, next) {
-    if (!session.userData.sentence) {
-      session.beginDialog('/changesentence');        
-    } else {
-      next();
-    }
-  },
-  function (session, results) {
-    session.send(session.userData.sentence);            
-  }
-]);
-bot.dialog('/changesentence',[
-  function (session) {
-    builder.Prompts.text(session, 'Hi, I am version 2.0 of the Microsoft Chatbot! I am already a lot smarter now! What would you like me to say?');
-  },
-  function (session, results) {
-    session.userData.sentence = results.response;
-    session.endDialog();
-  }
-])
+// var intents = new builder.IntentDialog();
+// bot.dialog('/', intents);
+// intents.matches(/^say something else/i, [
+//     function (session) {
+//         session.beginDialog('/changesentence');
+//     },
+//     function (session, results) {
+//         session.send('Ok, I will say the following sentence from now on: %s', session.userData.sentence);
+//     }
+// ])
+// intents.onDefault([
+//   function (session, args, next) {
+//     if (!session.userData.sentence) {
+//       session.beginDialog('/changesentence');        
+//     } else {
+//       next();
+//     }
+//   },
+//   function (session, results) {
+//     session.send(session.userData.sentence);            
+//   }
+// ]);
+// bot.dialog('/changesentence',[
+//   function (session) {
+//     builder.Prompts.text(session, 'Hi, I am version 2.0 of the Microsoft Chatbot! I am already a lot smarter now! What would you like me to say?');
+//   },
+//   function (session, results) {
+//     session.userData.sentence = results.response;
+//     session.endDialog();
+//   }
+// ])
+
+//'http://docs.botframework.com/builder/node/guides/understanding-natural-language/'
+
+
 
 if (useEmulator) {
     var restify = require('restify');
